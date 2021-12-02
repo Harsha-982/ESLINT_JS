7a3dfdb73fbc87ef6ae31293e4da0b293c82a38b
harsha <harshavardhini1702@gmail.com>
2021-12-02 14:25:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+for(let i=0;i<2;i++){
+    console.log("hi i am harsha");
+}
+//eslint-disable-next-line
+const sam='harsha';
---
+++ io.js
@@ -0,0 +1,8 @@
+function na  ( name ){
+    for(let i=0;i<2;i++){
+        console.log(name);
+    }   
+}
+
+const name=  " Harsha " ;
+na ( name );
